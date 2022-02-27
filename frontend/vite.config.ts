6c71547c99ec27f54218b7051fa0b1935b5cc7b4
seonpark <pksung1@gmail.com>
2022-02-28 01:25:19 +0900

    feat: runtimeConfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [adorableCSS(), react()]
+  plugins: [adorableCSS(), react()],
+  resolve: {
+    alias : { 
+      './runtimeConfig' : './runtimeConfig.browser'
+    }
+  },
 })
