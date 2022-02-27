77a331acba0d8c2567a5912d7e9f6f58b13b0580
seonpark <pksung1@gmail.com>
2022-02-27 20:48:45 +0900

    feat(FE) : initialize

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import "@adorable.css"
+import Tickets from './sections/Ticket'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Tickets />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/sections/Ticket.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tickets = () => {
+  return (
+    <div className='absolute top(0) bottom(0) w(100vw) h(100vh) bg(#fff) hbox pack'>
+      {/* TICKET */}
+      <div className='flex-1'></div>
+    </div>
+  )
+}
+
+export default Tickets
